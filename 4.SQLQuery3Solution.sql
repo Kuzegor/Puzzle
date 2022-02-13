340bcce7782bb1d4ee68aa7df38a7e0bd98cc32e
Kuzegor <kuznetcov03@inbox.ru>
2022-02-13 03:55:44 +0300

    SizeRenamedToSizeMB

--- 4.SQLQuery3Solution.sql
+++ 4.SQLQuery3Solution.sql
@@ -1,9 +1,9 @@
 USE Puzzle;
-UPDATE dbo.ListOfCourses SET Size = SUBSTRING(Courses,CHARINDEX('"",',Courses),len(Courses));
-UPDATE dbo.ListOfCourses SET Size = SUBSTRING(Size,4,len(Size)-10);
-UPDATE dbo.ListOfCourses SET Size = 1 WHERE ID = 222;
+UPDATE dbo.ListOfCourses SET SizeMB = SUBSTRING(Courses,CHARINDEX('"",',Courses),len(Courses));
+UPDATE dbo.ListOfCourses SET SizeMB = SUBSTRING(SizeMB,4,len(SizeMB)-10);
+UPDATE dbo.ListOfCourses SET SizeMB = 1 WHERE ID = 222;
 DELETE FROM dbo.ListOfCourses WHERE ID = 1 OR ID = 281;
-ALTER TABLE dbo.ListOfCourses ALTER COLUMN Size int;
+ALTER TABLE dbo.ListOfCourses ALTER COLUMN SizeMB int;
 
 DECLARE @count int
 SET @count = 0
@@ -11,7 +11,7 @@ DECLARE @sum int
 SET @sum = 0
 WHILE (@sum<5120)
 BEGIN
-	SET @sum += (SELECT Size FROM dbo.ListOfCourses WHERE ID = @count+2)
+	SET @sum += (SELECT SizeMB FROM dbo.ListOfCourses WHERE ID = @count+2)
 	SET @count += 1
 END
 PRINT 'Нужно удалить минимум ' + Convert(varchar(3),@count) +' курсов'
